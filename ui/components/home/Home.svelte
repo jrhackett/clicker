dc214e1672ae92dad0ed432fd3a6cdeb5354b889
Jacob Hackett <jacobrhackett@gmail.com>
2020-05-27 16:37:37 -0400

    continually update UI with player info

--- ui/components/home/Home.svelte
+++ ui/components/home/Home.svelte
@@ -1,27 +1,65 @@
 <script>
-import colors from 'styles/colors'
-    import { onMount } from "svelte";
+    import colors from 'styles/colors'
+    import { onMount } from 'svelte';
 
     let player;
 
     onMount(async () => {
-        await fetch(`/api/v1/player`)
-            .then(r => r.json())
-            .then(data => {
-                console.log(data)
-                player = data.player
-            })
+        await load()
     })
 
-    async function handleClick(name) {
-        await fetch(`/api/v1/buy`, {
+    const load = async () => await fetch('/api/v1/player')
+            .then(r => r.json())
+            .then(data => {
+                updatePlayer(data)
+                player.generators.forEach(async g => await updateGeneratorsCost(g.name))
+            })
+            .then(async () => setInterval(update, 200))
+
+    const update = async () => await fetch('/api/v1/player')
+            .then(r => r.json())
+            .then(data => updatePlayer(data))
+
+    const handleClick = async name => {
+        await fetch('/api/v1/buy', {
             method: 'POST',
             body: JSON.stringify({ name })
         })
             .then(r => r.json())
+            .then(async data => {
+                updatePlayer(data)
+                await updateGeneratorsCost(player.generators.find(a => a.name == name).name)
+            })
+    }
+
+    const updatePlayer = data => {
+        player = {
+            ...player,
+            liquid: data.player.liquid ? data.player.liquid : player.liquid,
+            generators: data.player.generators.map(g => ({
+                ...(player ? player.generators.find(a => a.name == g.name) : {}),
+                ...g
+            }))
+        }
+    }
+
+    const updateGeneratorsCost = async name => {
+        await fetch(`api/v1/cost?name=${ name }&count=1`)
+            .then(r => r.json())
             .then(data => {
-                console.log(data)
-                player = data.player
+                player = {
+                    ...player,
+                    generators: player.generators.map(g => {
+                        if (g.name == name) {
+                            return {
+                                ...g,
+                                cost: data.cost
+                            }
+                        }
+
+                        return g
+                    })
+                }  
             })
     }
 </script>
@@ -65,7 +103,7 @@ import colors from 'styles/colors'
                     <li on:click={ () => handleClick(generator.name) }>
                         <div class="inner" style="--tile-color:{ colors.blue };--text-color:{ colors.white }">
                             <h3>Name: { generator.name }</h3>
-                            <p>Cost: { generator.cost }</p>
+                            <p>Cost: { generator.cost && generator.cost.toFixed(0) }</p>
                             <p>Count: { generator.count }</p>
                             <p>Gained: { generator.gained.toFixed(0) }</p>
                             <p>Gain Per Second: { generator.gain_per_second }</p>
