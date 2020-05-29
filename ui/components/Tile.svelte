d855f4c9681b27bd0e1285ad0780710ed47f5420
Jacob Hackett <jacobrhackett@gmail.com>
2020-05-29 16:23:18 -0400

    restyling the tiles and adding some images for the generators

--- ui/components/Tile.svelte
+++ ui/components/Tile.svelte
@@ -17,6 +17,10 @@
             updatePlayer(data.player)
             await updateGeneratorsCost(name)
         })
+
+    $: buttonColor = player.liquid >= generator.cost ? colors.yellow : colors.lightGrey
+    $: buttonHoverColor = player.liquid >= generator.cost ? colors.brightYellow : colors.lightGrey
+    $: buttonSelectedColor = player.liquid >= generator.cost ? colors.dullYellow : colors.lightGrey
 </script>
 
 <style>
@@ -30,21 +34,72 @@
     }
 
     .tile {
+        display: flex;
         padding: 0.5rem 1rem;
         border-radius: 2px; 
         color: var(--text-color);
         background-color: var(--tile-color);
     }
+
+    .col {
+        display: flex;
+        flex-direction: column;
+        padding: 0 0.25rem;
+    }
+
+    h3 {
+        font-size: 1rem;
+    }
+
+    p, span {
+        font-size: 0.7rem;
+    }
+
+    span {
+        font-weight: bold;
+    }
+
+    button {
+        background-color: var(--button-color);
+        border: none;
+    }
+
+    button:active, button:focus {
+        outline: none;
+    }
+
+    button:hover {
+        background-color: var(--button-hover-color);
+    }
+
+    button:active {
+        background-color: var(--button-selected-color);
+    }
 </style>
 
-<li on:click={ () => handleClick(generator.name) }>
+<li>
     <div class="inner">
-        <div class="tile" style="--tile-color:{ player.liquid >= generator.cost ? colors.blue : colors.lightGrey };--text-color:{ colors.white }">
-            <h3>{ generator.name }</h3>
-            <p>Cost: { generator.cost && generator.cost.toFixed(0) } likes</p>
-            <p>Owned: { generator.count }</p>
-            <p>ROI: { generator.gained.toFixed(0) }</p>
-            <p>Yield: { generator.gain_per_second }</p>
+        <div class="tile" style="--tile-color:{ colors.blue };--text-color:{ colors.black }">
+            <div class="col">
+                <h3>{ generator.name }</h3>
+                {#if !!generator.image_data_uri}
+                    <img src={ generator.image_data_uri } alt={ generator.name } />
+                {/if}
+            </div>
+            <div>
+                <p>Cost: <span>{ generator.cost && generator.cost.toFixed(0) } likes</span></p>
+                {#if generator.count > 0}
+                    <p>Owned: <span>{ generator.count }</span></p>
+                    <p>Earned: <span>{ generator.gained.toFixed(0) }</span></p>
+                {/if}
+                <p>Yield: <span>{ generator.gain_per_second }</span></p>
+                <button
+                    on:click={ () => handleClick(generator.name) }
+                    style="--button-color:{ buttonColor };--button-hover-color:{ buttonHoverColor };--button-selected-color:{ buttonSelectedColor }"
+                >
+                    Buy
+                </button>
+            </div>
         </div>
     </div>
 </li>
--- ui/styles/colors.js
+++ ui/styles/colors.js
@@ -1,10 +1,12 @@
 export default {
-    black: '#272838',
+    black: '#000000',
     blue: '#00E4F3',
     grey: '#606060',
     lightBlue: '#8F97FC',
-    lightGrey: '#989CCE',
+    lightGrey: '#B5B5B5',
     yellow: '#EFF48F',
+    brightYellow: '#FFF800',
+    dullYellow: '#B7B739',
     orange: '#FF935C',
     offWhite: '#efefef',
     white: '#ffffff'
